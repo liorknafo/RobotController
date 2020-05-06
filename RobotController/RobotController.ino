153b54f295539b82e368acae8d79a1503c1986db
lior <liorknafo@gmail.com>
2020-05-07 00:15:50 +0300

    fix the motor micro steps

--- RobotController/RobotController.ino
+++ RobotController/RobotController.ino
@@ -2,20 +2,27 @@
 #include "Joystic.h"
 #include "Motor.h"
 
-Motor motor1(2, 3, 6, STEPS);
-Motor motor2(4, 5, 7, STEPS);
+Motor motor1(3, 2, 6, STEPS, 1.8, 1 / 4.f);
+Motor motor2(4, 5, 7, STEPS, 1.8, 1 / 4.f);
 Joystic joystic(A0, A1);
+
+float speed = 1.0f;
+unsigned long stepsPerRevolution = 360.f / (1.8f * 0.5f);
+unsigned long delay_time = (long)(1E6 / (stepsPerRevolution * speed));
 
 void setup() {
     // initialize digital pin 13 as an output.
     motor1.setup();
     motor2.setup();
 
-    motor1.setSpeed(0.75f);
-    motor2.setSpeed(0.75f);
+    motor1.setSpeed(0.5f);
+    motor2.setSpeed(0.5f);
     Serial.begin(9600);
     //motor1.setRotation(135);
+    //motor2.setRotation(135);
 }
+
+unsigned long lastTimeHigh = 0;
 
 // the loop function runs over and over again forever
 void loop() {
@@ -29,4 +36,22 @@ void loop() {
 
     motor1.setRotation(x * 90);
     motor2.setRotation(y * 90);
+
+
+    /*digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    delayMicroseconds(300);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    delayMicroseconds(300);*/
+    //Serial.println(delay_time);
+
+    /*unsigned long now = micros();
+    if (now - lastTimeHigh > delay_time) {
+        lastTimeHigh = now;
+        digitalWrite(2, HIGH);
+    }
+    else if (now - lastTimeHigh > delay_time / 2) {
+        digitalWrite(2, LOW);
+    }*/
 }
